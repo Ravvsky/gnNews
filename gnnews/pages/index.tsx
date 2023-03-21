60b12944258968eea98bb9e282cf199313621115
Ravvsky <pawel.rawsk@gmail.com>
2023-03-21 23:54:49 +0100

    feat: add SideMenu to index page

--- gnnews/pages/index.tsx
+++ gnnews/pages/index.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import SideMenu from "@/components/SideMenu/SideMenu";
 
 export default function Home() {
   return (
     <>
       <div className="bg-neutral-100 h-screen text-white flex flex-col bg justify-between">
         <Header />
+        <SideMenu />
         <Footer />
       </div>
     </>
