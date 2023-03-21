170c5c6c9a982070dd5a925d5d77d389683ac877
Ravvsky <pawel.rawsk@gmail.com>
2023-03-21 23:37:36 +0100

    feat: add Footer component

---
+++ gnnews/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Container from "../Container/Container";
+
+const Footer = () => {
+  return (
+    <div className="bg-lime-200 min-h-[20rem]">
+      <Container>Footer</Container>
+    </div>
+  );
+};
+
+export default Footer;
