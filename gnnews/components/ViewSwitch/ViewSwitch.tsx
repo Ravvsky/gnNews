35b747f862925e37c73265766d091fbc072d5608
Ravvsky <pawel.rawsk@gmail.com>
2023-03-21 23:37:56 +0100

    feat: add ViewSwitch component

---
+++ gnnews/components/ViewSwitch/ViewSwitch.tsx
@@ -0,0 +1,19 @@
+import "material-icons/iconfont/material-icons.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setView } from "@/store/reducers";
+const ViewSwitch = () => {
+  const dispatch = useDispatch();
+  const icon = useSelector((state: { view: string }) => state.view);
+
+  const switchHandler = () => {
+    dispatch(setView());
+  };
+
+  return (
+    <button onClick={switchHandler} className="flex items-center">
+      <span className="material-icons">{icon}</span>
+    </button>
+  );
+};
+
+export default ViewSwitch;
