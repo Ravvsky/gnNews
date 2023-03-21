db264a9c6623d538ef0dd2c3744f4da6dbafc051
Ravvsky <pawel.rawsk@gmail.com>
2023-03-21 23:38:20 +0100

    feat: Add Header and footer to index page

--- gnnews/pages/index.tsx
+++ gnnews/pages/index.tsx
@@ -1,3 +1,13 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-neutral-100 h-screen text-white flex flex-col bg justify-between">
+        <Header />
+        <Footer />
+      </div>
+    </>
+  );
 }
