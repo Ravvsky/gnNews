1653ddb048bd3bba8426e6d8b3bdcf9ad07e7cf2
Ravvsky <pawel.rawsk@gmail.com>
2023-03-21 23:54:36 +0100

    feat: create SideMenu component

---
+++ gnnews/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,13 @@
+import Container from "../Container/Container";
+
+const SideMenu = () => {
+  return (
+    <Container className="h-full my-[2rem]">
+      <div className="h-full bg-red-500 w-[22rem] rounded-[1rem] p-[2rem] text-center font-medium">
+        Wybierz kraj
+      </div>
+    </Container>
+  );
+};
+
+export default SideMenu;
