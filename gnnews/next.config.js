084650b0c792766dfb07312204ab74a3c84a1592
Ravvsky <pawel.rawsk@gmail.com>
2023-03-28 20:02:11 +0200

    feat: add support for images from any domain

--- gnnews/next.config.js
+++ gnnews/next.config.js
@@ -1,6 +1,24 @@
+const { hostname } = require("os");
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.si.com",
+        port: "",
+        pathname: "/.image/**",
+      },
+    ],
+  },
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
