a9dffdd63fa71250cf6a60a52545da819827d0d9
Ravvsky <pawel.rawsk@gmail.com>
2023-03-28 20:58:22 +0200

    feat: add translation to 404 page

--- gnnews/pages/404.tsx
+++ gnnews/pages/404.tsx
@@ -2,9 +2,14 @@ import Button from "@/components/Button/Button";
 import Container from "@/components/Container/Container";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { useTranslation } from "next-i18next";
 import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Page404 = () => {
+  const { t } = useTranslation("common");
+
+  console.log(t("readMore"));
   return (
     <div className="bg-neutral-100 h-screen text-white flex flex-col bg justify-between">
       <Header />
@@ -17,7 +22,7 @@ const Page404 = () => {
             type="button"
             className="w-max p-[2rem] bg-lime-300 text-neutral-800	 hover:bg-lime-600 hover:text-neutral-50"
           >
-            Back to homepage
+            {t("backtoHomePage")}
           </Button>
         </Link>
       </Container>
@@ -27,3 +32,11 @@ const Page404 = () => {
 };
 
 export default Page404;
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "footer"])),
+    },
+  };
+}
