0ad98c696446bb4348b2724f2343c1d137aff1cd
Ravvsky <pawel.rawsk@gmail.com>
2023-03-28 20:18:27 +0200

    feat: add content to Footer component

--- gnnews/components/Footer/Footer.tsx
+++ gnnews/components/Footer/Footer.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
 import Container from "../Container/Container";
+import { useSelector } from "react-redux";
+import { useTranslation } from "next-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation("footer");
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString("pl-pl", {
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+  );
+  useEffect(() => {
+    setInterval(() => {
+      setTime(
+        new Date().toLocaleTimeString("pl-pl", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+    }, 1000);
+  }, []);
+
+  const visibleArticles = useSelector(
+    (state: { visibleArticles: number }) => state.visibleArticles
+  );
   return (
-    <div className="bg-lime-200 min-h-[20rem]">
-      <Container>Footer</Container>
-    </div>
+    <footer className="bg-lime-300 min-h-[20rem] flex">
+      <Container className="flex justify-between items-center text-black w-full">
+        <div>Ilosć artykułów na stronie głównej: {`${visibleArticles}`}</div>
+
+        <div>{`${t("currentTime")} ${time}`}</div>
+      </Container>
+    </footer>
   );
 };
 
